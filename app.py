4e9ec334f8570dede87e9d9a1f25876e5e7e4aec
Eli Jones <ejones@gilt.com>
2018-02-16 10:48:39 -0500

    app.py: adding some commentary to /api/receive endpoint.

--- app.py
+++ app.py
@@ -36,6 +36,26 @@ def hello():
 
 @app.route('/api/receive', methods=['POST'])
 def receive():
+    """This is a fairly simple approach to letting people set up destination_addresses
+    and to get a new deposit address without providing any additional information.
+
+    IF denial of service attacks became a problem, one could institute a dynamic
+    proof-of-work requirement for POSTs to this endpoint.
+
+    Something like:
+    
+        hash = SHA256("<timestamp>-<json_serialized_destination_addresses>-<some_number>)
+
+    Verified with:
+
+        hash.startswith("0000") and is_kosher(request.json['timestamp'])
+
+    Or whatnot, depending on the client side rate limiting we desire.
+
+    This could suck for the "good actors", but feels like a good approach to explore
+    since good actors shouldn't need to be perpetually creating new deposit, destinations
+    pairings.
+    """
     if not request.json or not 'addresses' in request.json:
         abort(400)
 
